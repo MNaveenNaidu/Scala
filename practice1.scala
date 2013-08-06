c8af2d9554d3f67ca39ccf6ec785dfd2758b45b9
M Naveen Naidu <m.naveen.naidu@live.com>
2013-08-07 01:09:46 +0530

    simple scala programs

---
+++ practice1.scala
@@ -0,0 +1,7 @@
+object practice1 {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(86); 
+  def Rational(x:Int,y:Int) = {
+  def numer = x
+  def demon = y
+  };System.out.println("""Rational: (x: Int, y: Int)Unit""");$skip(29); 
+  val a  = new Rational(1,2);System.out.println("""a  : <error> = """ + $show(a ))}
+}
